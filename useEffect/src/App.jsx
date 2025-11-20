86eb17964446cab7d6818f1fd612e8dd0e06edd3
hetuk2005 <patelhetuk2005@gmail.com>
2025-11-20 14:54:15 +0530

    Code

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+
+  useEffect(() => {
+    console.log("Hello Without Dependency");
+  });
+
+  useEffect(() => {
+    console.log("Hello With Dependency But Empty");
+  }, []);
+
+  useEffect(() => {
+    console.log("Hello With Dependency");
+  }, [count]);
+
+  return (
+    <>
+      <div className="App">
+        <h2>Count = {count}</h2>
+        <h2>Count 1 = {count1}</h2>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <button onClick={() => setCount1((prev) => prev + 1)}>+ count1</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
