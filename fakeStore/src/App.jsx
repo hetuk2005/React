ce2b0479339bb129fc9ee077c47e524bcb7e1585
hetuk2005 <patelhetuk2005@gmail.com>
2025-11-20 21:01:47 +0530

    Code

---
+++ fakeStore/src/App.jsx
@@ -0,0 +1,11 @@
+import { Api_Calling } from "./Components/Api_Calling";
+
+function App() {
+  return (
+    <>
+      <Api_Calling />
+    </>
+  );
+}
+
+export default App;
---
+++ fakeStore/src/Components/Api_Calling.jsx
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const Api_Calling = () => {
+  const API = "https://fakestoreapi.com/products";
+
+  const [data, setData] = useState([]);
+
+  const getApi = () => {
+    axios
+      .get(API)
+      .then((res) => setData(res.data))
+      .catch((err) => console.log("Error: ", err))
+      .finally(() => console.log("Done"));
+  };
+  console.table(data);
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  return (
+    <>
+      <h1>Fake Store</h1>
+      <button>Click Me</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4,1fr)",
+          justifyItems: "center",
+          alignContent: "center",
+          gap: "41px",
+          padding: "21px",
+        }}
+      >
+        {data.map((el) => (
+          <div
+            key={el.id}
+            style={{
+              border: "2px",
+              borderStyle: "solid",
+              width: "100%",
+              padding: "15px",
+              borderRadius: "7px",
+            }}
+          >
+            <img
+              src={el.image}
+              alt="Images"
+              style={{
+                width: "100%",
+                height: "200px",
+                objectFit: "contain",
+                borderTopRightRadius: "7px",
+                borderTopLeftRadius: "7px",
+              }}
+            />
+            <h5>ID: {el.id}</h5>
+            <h5>Title: {el.title}</h5>
+            <h5 style={{ textTransform: "capitalize" }}>
+              Category: {el.category}
+            </h5>
+            <h5>Description: {el.description}</h5>
+            <h5>Price: {el.price}</h5>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "left",
+                alignItems: "center",
+                gap: "30px",
+              }}
+            >
+              <h5>Rate: {el.rating.rate}</h5>
+              <h5>Count: {el.rating.count}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
