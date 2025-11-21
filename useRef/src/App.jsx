ea465a5092ec61284b0ee8cbae5c7e40260799f5
hetuk2005 <patelhetuk2005@gmail.com>
2025-11-21 16:32:36 +0530

    Code

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,11 +1,31 @@
+import { useRef } from "react";
 
 function App() {
+  const defaultData = useRef(null);
+
+  const handleInput = () => {
+    const textVal = defaultData.current.value;
+    console.log("Text Value: ", textVal);
+  };
+
+  // const [text, setText] = useState("");
+  // console.log("Text: ", text);
+
+  console.log("My App Render");
 
   return (
     <>
-      
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Enter Some Data"
+          ref={defaultData}
+          // onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={handleInput}>Click</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
