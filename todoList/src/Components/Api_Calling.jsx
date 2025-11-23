61e538ed14bfd1e0a095b3f9fce6c29f09b9739b
hetuk2005 <patelhetuk2005@gmail.com>
2025-11-23 13:19:07 +0530

    Code

--- todoList/src/Components/Api_Calling.jsx
+++ todoList/src/Components/Api_Calling.jsx
@@ -5,6 +5,7 @@ export const Api_Calling = () => {
   const API = "https://react-8zz0.onrender.com/todo";
 
   const [data, setData] = useState([]);
+  const [checkedItems, setCheckedItems] = useState([]);
   const [input, setInput] = useState("");
   const [editValueID, setEditValueID] = useState("");
   const [editValue, setEditValue] = useState("");
@@ -87,10 +88,109 @@ export const Api_Calling = () => {
     setData(cancelArr);
   };
 
+  const handleChangeComplete = async (value, id) => {
+    if (!id) return;
+    await axios.patch(`${API}/${id}`, { complete: value });
+
+    const checkedArr = data.map((el) =>
+      el.id === id ? { ...el, complete: value } : el
+    );
+
+    const onlyChecked = checkedArr.filter((el) => el.complete === true);
+    setCheckedItems(onlyChecked);
+    setData(checkedArr);
+  };
+
+  const handleAllClick = async () => {
+    for (let item of data) {
+      await axios.patch(`${API}/${item.id}`, { complete: true });
+    }
+
+    const setAllTrue = data.map((el) => {
+      return { ...el, complete: true };
+    });
+
+    setData(setAllTrue);
+    setCheckedItems(setAllTrue);
+  };
+
+  const handleAllDeSelect = async () => {
+    for (let item of data) {
+      await axios.patch(`${API}/${item.id}`, { complete: false });
+    }
+
+    const setAllFalse = data.map((el) => {
+      return { ...el, complete: false };
+    });
+
+    setData(setAllFalse);
+    setCheckedItems([]);
+  };
+
+  const handleAllDelete = async () => {
+    // await axios.patch(`${API}/${editValueID}`, { complete: false });
+    for (let item of data) {
+      if (item.complete) {
+        await axios.delete(`${API}/${item.id}`);
+      }
+    }
+
+    const deleteArr = data.filter((el) => !el.complete);
+    setData(deleteArr);
+    setCheckedItems([]);
+  };
+
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Todo Input</h1>
       <br />
+      {checkedItems.length <= 0 ? (
+        <button
+          style={{
+            position: "absolute",
+            right: "500px",
+            top: "230px",
+            cursor: "pointer",
+            borderRadius: "5px",
+            border: "1px",
+            borderStyle: "solid",
+          }}
+          onClick={handleAllClick}
+        >
+          Select All
+        </button>
+      ) : (
+        <>
+          <button
+            style={{
+              position: "absolute",
+              right: "580px",
+              top: "230px",
+              cursor: "pointer",
+              borderRadius: "5px",
+              border: "1px",
+              borderStyle: "solid",
+            }}
+            onClick={handleAllDeSelect}
+          >
+            De-Select
+          </button>
+          <button
+            style={{
+              position: "absolute",
+              right: "500px",
+              top: "230px",
+              cursor: "pointer",
+              borderRadius: "5px",
+              border: "1px",
+              borderStyle: "solid",
+            }}
+            onClick={() => handleAllDelete()}
+          >
+            Delete All
+          </button>
+        </>
+      )}
       <div
         style={{
           display: "flex",
@@ -104,43 +204,123 @@ export const Api_Calling = () => {
           placeholder="Enter Task"
           onChange={(e) => setInput(e.target.value)}
           value={input}
+          style={{
+            padding: "5px",
+            borderRadius: "5px",
+            border: "1px",
+            borderStyle: "solid",
+          }}
         />
-        <button onClick={handleAdd}>Add</button>
+        <button
+          onClick={handleAdd}
+          style={{
+            cursor: "pointer",
+            border: "1px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+          }}
+        >
+          Add
+        </button>
       </div>
       <br />
       <br />
       <h1 style={{ textAlign: "center" }}>Todo List</h1>
-      {data === 0 && <h1>No Task</h1>}
+      <br />
+      {data <= 0 && <h1 style={{ textAlign: "center" }}>No Task</h1>}
       <br />
       {data?.map((el) => (
         <div
           key={el.id}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "0 30px",
-            gap: "50px",
-          }}
+          style={
+            el.complete
+              ? {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "0 30px",
+                  gap: "50px",
+                  opacity: "0.5",
+                }
+              : {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "0 30px",
+                  gap: "50px",
+                }
+          }
         >
+          <input
+            type="checkbox"
+            checked={el.complete}
+            id="check"
+            onChange={(e) => handleChangeComplete(e.target.checked, el.id)}
+            style={{ cursor: "pointer", borderRadius: "5px" }}
+          />
           {el.edit ? (
             <input
               type="text"
+              style={{ borderRadius: "5px" }}
               value={editValue}
               onChange={(e) => setEditValue(e.target.value)}
             />
           ) : (
-            <h3>{el.task}</h3>
+            <h3
+              style={{ textDecoration: el.complete ? "line-through" : "none" }}
+            >
+              {el.task}
+            </h3>
           )}
           {el.edit ? (
             <>
-              <button onClick={handleEditComfrim}>Confirm</button>
-              <button onClick={() => handleEditCancel(el.id)}>Cancel</button>
+              <button
+                onClick={handleEditComfrim}
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "5px",
+                  border: "1px",
+                  borderStyle: "solid",
+                }}
+              >
+                Confirm
+              </button>
+              <button
+                onClick={() => handleEditCancel(el.id)}
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "5px",
+                  border: "1px",
+                  borderStyle: "solid",
+                }}
+              >
+                Cancel
+              </button>
             </>
           ) : (
             <>
-              <button onClick={() => handleEdit(el.id)}>Edit</button>
-              <button onClick={() => handleDelete(el.id)}>Delete</button>
+              <button
+                onClick={() => handleEdit(el.id)}
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "5px",
+                  border: "1px",
+                  borderStyle: "solid",
+                }}
+              >
+                Edit
+              </button>
+              <button
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "5px",
+                  border: "1px",
+                  borderStyle: "solid",
+                }}
+                onClick={() => handleDelete(el.id)}
+              >
+                Delete
+              </button>
             </>
           )}
         </div>
