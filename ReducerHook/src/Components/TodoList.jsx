6d4fe9476edb080468596851a7de2c55dedba8a9
hetuk2005 <patelhetuk2005@gmail.com>
2025-12-18 15:27:01 +0530

    Code

--- ReducerHook/src/Components/TodoList.jsx
+++ ReducerHook/src/Components/TodoList.jsx
@@ -2,6 +2,37 @@ import React from "react";
 
 import * as type from "../Reducer/Action";
 
-export const TodoList = () => {
+export const TodoList = ({ values }) => {
   const { state, dispatch } = values;
+
+  const handleDelete = (id) => {
+    dispatch({ type: type.DELETE_TODO_ITEMS, payload: id });
+  };
+
+  return (
+    <>
+      {state.items &&
+        state.items.map((el) => {
+          return (
+            <ul
+              key={el.id}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                listStyle: "none",
+              }}
+            >
+              <input type="checkbox" />
+              <li>{el.id}</li>
+              <li>{el.text}</li>
+              <div style={{ display: "flex", gap: "11px" }}>
+                <button>Edit</button>
+                <button onClick={() => handleDelete(el.id)}>Delete</button>
+              </div>
+            </ul>
+          );
+        })}
+    </>
+  );
 };
--- ReducerHook/src/Reducer/Reducer.js
+++ ReducerHook/src/Reducer/Reducer.js
@@ -1,1 +1,29 @@
 import { ADD_TODO_ITEMS, DELETE_TODO_ITEMS, EDIT_TODO_ITEMS } from "./Action";
+import { initialValue } from "./Store";
+
+export const reducer = (state = initialValue, action) => {
+  switch (action.type) {
+    case ADD_TODO_ITEMS:
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            id: Date.now(),
+            text: action.payload,
+            isEdits: false,
+            isComplete: false,
+          },
+        ],
+      };
+
+    case DELETE_TODO_ITEMS:
+      return {
+        ...state,
+        items: state.items.filter((el) => el.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
